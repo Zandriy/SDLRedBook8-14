77dc21c073418126a3ae732aac04dfbc542ac613
Andanat_Mac <andgen@list.ru>
2012-11-10 12:24:54 +0200

    coments are translated

--- src/Lesson_5_8.cpp
+++ src/Lesson_5_8.cpp
@@ -35,45 +35,46 @@ void Lesson_5_8::drawGLScene()
 {
 	glClearColor(0.0,0.0,0.0,0.0);
 	glShadeModel(GL_SMOOTH);
-	//Настраиваем источник света
+
+	//Set the light
 	GLfloat light_position[]={1.0,1.0,1.0,0.0};
 	GLfloat white_light[]={1.0,1.0,1.0,1.0};
 	glLightfv(GL_LIGHT0,GL_POSITION,light_position);
 	glLightfv(GL_LIGHT0,GL_DIFFUSE,white_light);
 	glLightfv(GL_LIGHT0,GL_SPECULAR,white_light);
 
-	//...и модель двухстороннего освещения
+	//...and model of two side lighting
 	GLfloat lmodel_ambient[]={1.0,1.0,1.0,1.0};
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT,lmodel_ambient);
 	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
 
-	//Включаем нужные механизмы
+	//Switch on the necessary things
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_DEPTH_TEST);
 
-	//Задаем параметры материала для лицевых граней
+	//Set material for facĕ
 	GLfloat mat_specular_front[]={0.0,0.3,0.9,1.0};
 	GLfloat mat_ambient_front[]={0.0,0.1,0.3,1.0};
 	glMaterialfv(GL_FRONT,GL_SPECULAR,mat_specular_front);
 	glMaterialfv(GL_FRONT,GL_AMBIENT,mat_ambient_front);
 
-	//... и для обратных
+	//... and for back
 	GLfloat mat_specular_back[]={1.0,0.0,0.0,1.0};
 	GLfloat mat_ambient_back[]={0.5,0.1,0.1,1.0};
 	glMaterialfv(GL_BACK,GL_SPECULAR,mat_specular_back);
 	glMaterialfv(GL_BACK,GL_AMBIENT,mat_ambient_back);
 
-	//GL_SHININESS в данном случае одинаково для всех граней
+	//GL_SHININESS is the same for both sides in this casĕ
 	GLfloat mat_shininess[]={50.0};
 	glMaterialfv(GL_FRONT_AND_BACK,GL_SHININESS,mat_shininess);
 
-	//Задаем дополнительную плоскость отсечения
+	//Set the plain of cutting
 	GLdouble equation[]={0.4,0.5,-0.5,0.6};
 	glClipPlane(GL_CLIP_PLANE1, equation);
 	glEnable(GL_CLIP_PLANE1);
 
-	//Задаем какие грани считать лицевыми
+	//Set which sides are face
 	glFrontFace(GL_CW);
 
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
